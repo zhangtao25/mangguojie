88b9350aec6a9e730184b540aadcad1710312f1e
zhangtao25 <zhangtao25@lenovo.com>
2018-08-12 17:20:02 +0800

    2018/8/12

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <router-view></router-view>
+</template>
+<style>
+  .margin{margin: 0 auto;width: 1200px}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import home from './../view/home'
+import login from './../view/login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: login
+    }
+  ]
+})
